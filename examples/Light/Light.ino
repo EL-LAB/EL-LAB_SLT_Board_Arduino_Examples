ffdb0747e9fc3472cdc8e64c028959ed66ba5fc6
D-Villa <d.villalobos8@gmail.com>
2018-10-12 14:28:28 -0600

    Update Light.ino

--- examples/Light/Light.ino
+++ examples/Light/Light.ino
@@ -15,12 +15,55 @@
 */
 
 
-void setup()
-{
+unsigned int photocellReading; // The analog reading from LDR
 
+void setup() 
+{
+  Serial.begin(9600);
+    
+  Serial.print("LIGHT LEVEL\t");
+  Serial.println("VOLTS");
+  Serial.println("------------------------------------------");
+    
 }
 
-void loop()
+void loop() 
 {
-
+  photocellReading = analogRead(1); // A1 analogue pin
+    
+  double volts = (photocellReading * 5.0) / 1024;  // Convert to volts
+    
+// Light level detected
+  if (photocellReading < 10) 
+  {
+    // Prints output message
+     Serial.println("Very bright\t");
+     Serial.println(volts);
+  }
+  else if (photocellReading < 200) 
+  {
+    // Prints output message
+    Serial.println("Bright\t");
+    Serial.println(volts);
+  }
+  else if (photocellReading < 500) 
+  {
+    // Prints output message
+    Serial.println("Light\t");
+    Serial.println(volts);
+  } 
+  else if (photocellReading < 800) 
+  {
+    // Prints output message
+    Serial.println("Very Dim\t");
+    Serial.println(volts);
+  } 
+  else 
+  {
+    // Prints output message
+    Serial.println("Dark\t");
+    Serial.println(volts);
+  }
+    
+  delay(500);
 }
