8e7bcc64f4d91841f7f48ec37f8e45f6e0c8166d
D-Villa <d.villalobos8@gmail.com>
2018-10-12 14:36:35 -0600

    Update Sound.ino

--- examples/Sound/Sound.ino
+++ examples/Sound/Sound.ino
@@ -27,7 +27,7 @@
 
 
 const int sampleWindow = 250; // Sample window width in mS (250 mS = 4Hz)
-unsigned int sound;
+unsigned int soundReading;    // The analog reading from microphone
 int ledPin = 13;
 
 void setup() 
@@ -35,7 +35,7 @@ void setup()
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
     
-  Serial.print("SOUND\t");
+  Serial.print("SOUND LEVEL\t");
   Serial.println("VOLTS");
   Serial.println("------------------------------------------");
 }
@@ -51,34 +51,35 @@ void loop()
  // Collect data for 250 miliseconds
  while (millis() - start < sampleWindow)
  {
-   sound = analogRead(0);
-      if (sound < 1024)  //This is the max of the 10-bit ADC so this loop will include all readings
-      {
-         if (sound > signalMax)
-         {
-           signalMax = sound;  // Save only the max levels
-         }
-      else if (sound < signalMin)
-         {
-           signalMin = sound;   // Save only the min levels
-         }
-     }
+   soundReading = analogRead(0); // A0 analog pin
+     
+    if (soundReading < 1024)  //This is the max of the 10-bit ADC so this loop will include all readings
+    {
+       if (soundReading > signalMax)
+       {
+         signalMax = soundReading;  // Save only the max levels
+       }
+    else if (soundReading < signalMin)
+       {
+         signalMin = soundReading;   // Save only the min levels
+       }
+   }
  }
     
  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
  
  double volts = (peakToPeak * 3.3) / 1024;  // Convert to volts
  
-if (volts >= 1.0)
-{
-  // Turns LED on and prints output message
-  digitalWrite(ledPin, HIGH);
-  Serial.print("Sound detected\t");
-  Serial.println("volts");
-}
-else
-{
-  //turn LED off
-  digitalWrite(ledPin, LOW);
-}             
+  if (volts >= 1.0)
+  {
+    // Turns LED on and prints output message
+    digitalWrite(ledPin, HIGH);
+    Serial.print("Sound detected\t");
+    Serial.println(volts);
+  }
+  else
+  {
+    // Turns LED off
+    digitalWrite(ledPin, LOW);
+  }             
 }
