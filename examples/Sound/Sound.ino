8ad4f1d3fca1ce835d88bcc52f8e86480fec38f8
D-Villa <d.villalobos8@gmail.com>
2018-10-12 11:59:12 -0600

    Create Sound.ino

---
+++ examples/Sound/Sound.ino
@@ -0,0 +1,26 @@
+/*
+    Sound.ino
+
+    This example shows how to use the sound sensor on the 
+    platform EL LAB: SLT Board.
+
+    The sound sensor used is a standard PCB mount microphone.
+
+    Created: October 12th, 2018
+    Author: El Lab de Electro: EL LAB
+    Modified: -
+    Author: -
+
+    URL: https://github.com/EL-LAB/EL-LAB_SLT_Board_Arduino_Examples
+*/
+
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
