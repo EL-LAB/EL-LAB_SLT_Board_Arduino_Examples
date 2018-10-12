d8bd4823621e7a4c8ef9a45fc24bd4890978af63
D-Villa <d.villalobos8@gmail.com>
2018-10-12 12:04:55 -0600

    Create Temperature.ino

---
+++ examples/Temperature/Temperature.ino
@@ -0,0 +1,26 @@
+/*
+    Temperature.ino
+
+    This example shows how to use the temperature sensor on the 
+    platform EL LAB: SLT Board.
+
+    The temperature sensor used is the LM335Z.
+
+    Created: October 12th, 2018
+    Author: El Lab de Electro: EL LAB
+    Modified: -
+    Author: -
+
+    URL: https://github.com/EL-LAB/EL-LAB_SLT_Board_Arduino_Examples
+*/
+
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
