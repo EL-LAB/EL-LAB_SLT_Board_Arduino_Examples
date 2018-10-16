05ec4908330968568238a36c72dfd574ff581ec8
D-Villa <d.villalobos8@gmail.com>
2018-10-15 19:55:08 -0600

    Update Light.ino

--- examples/Light/Light.ino
+++ examples/Light/Light.ino
@@ -1,16 +1,16 @@
 /*
     Light.ino
-
+    
     This example shows how to use the light sensor on the 
     platform EL LAB: SLT Board.
-
+    
     The light sensor used is a standard LDR (photoresistor).
-
+    
     Created: October 12th, 2018
     Author: El Lab de Electro: EL LAB
     Modified: -
     Author: -
-
+    
     URL: https://github.com/EL-LAB/EL-LAB_SLT_Board_Arduino_Examples
 */
 
