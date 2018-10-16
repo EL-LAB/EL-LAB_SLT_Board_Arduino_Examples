3230b930158a790a387692aee6d9cfa6c6521237
D-Villa <d.villalobos8@gmail.com>
2018-10-15 20:11:39 -0600

    Update Temperature.ino

--- examples/Temperature/Temperature.ino
+++ examples/Temperature/Temperature.ino
@@ -1,19 +1,19 @@
 /*
     Temperature.ino
-
+    
     This example shows how to use the temperature sensor on the 
     platform EL LAB: SLT Board.
-
+    
     The temperature sensor used is the LM335Z and 
     it's calibrated at 10mV/K.
     
     Calibrated for 2.982V at 25°C.
-
+    
     Created: October 12th, 2018
     Author: El Lab de Electro: EL LAB
     Modified: -
     Author: -
-
+    
     URL: https://github.com/EL-LAB/EL-LAB_SLT_Board_Arduino_Examples
 */
 
@@ -35,11 +35,11 @@ void loop()
     
   double volts = (temperatureReading * 5.0) / 1024;  // Convert to volts
   double tempKelvin =  volts * 100;                  // Temperature in Kelvin (10mV/K)
-  double tempCelsius = tempK - 273.15;               // Temperature in Celsius 
+  double tempCelsius = tempKelvin - 273.15;          // Temperature in Celsius 
  
   // Prints output message
   Serial.print(tempCelsius);
-  Serial.print("\t");
+  Serial.print("\t\t");
   Serial.println(volts);
  
   delay(1000);
