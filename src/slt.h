b792c4cb6c4d85eb47bb1579393250b9ba101a83
D-Villa <d.villalobos8@gmail.com>
2018-10-15 20:17:12 -0600

    Create slt.h

---
+++ src/slt.h
@@ -0,0 +1,17 @@
+/***********************************************************************
+* FILENAME: slt.h
+*
+* DESCRIPTION:
+*       This library only contains examples based on analogue reads 
+*       functions.
+*
+* URL:  https://github.com/EL-LAB/EL-LAB_SLT_Board_Arduino_Examples
+* 
+* AUTHOR:  Diego Villalobos    START DATE :  October 15 2018
+*
+* CHANGES:
+*
+* VERSION      DATE            WHO         DETAIL
+*  1.0.0   October 15 2018   Diego V.      Initial release
+*
+***********************************************************************/
